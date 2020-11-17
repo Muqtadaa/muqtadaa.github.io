8f0db77cbf29c9e0e91877ecb4d1625916681547
Muqtadaa Miandara <muqdara95@gmail.com>
2020-11-17 00:07:54 -0600

    First Push
    Here we go

---
+++ script.js
@@ -0,0 +1,16 @@
+// JavaScript Document
+$(document).ready(function(){
+  $(function() {
+    
+    $(".name").on("click", function(){
+      $(".photo").slideToggle(); 
+    });
+    
+    $("#skillsBtn").on("click", function(){
+      $("#downBTN").slideToggle();
+      $("#infoRow").slideToggle();
+      $("#upBTN").slideToggle();
+    })
+    
+  });  
+});
