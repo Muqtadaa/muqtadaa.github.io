b7daa3cf6d6eb5ce5f573183fef355992440bc0d
Muqtadaa Miandara <muqdara95@gmail.com>
2020-11-18 22:29:30 -0600

    homepage edits
    added icons for skills and fixed image icons on other works

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
 // JavaScript Document
 $(document).ready(function(){
+  
   $(function() {
     
     $(".name").on("click", function(){
+  
       $(".photo").slideToggle(); 
+    
     });
     
     $("#skillsBtn").on("click", function(){
-      $("#downBTN").slideToggle();
-      $("#infoRow").slideToggle();
-      $("#upBTN").slideToggle();
-    })
     
-  });  
+      $("#downBTN").slideToggle();
+      
+      $("#infoRow").slideToggle();
+      
+      $("#upBTN").slideToggle();
+    
+    });
+    
+  });
+  
 });
