1850b2e30b940b390ab895fce83c5d34337092d0
Muqtadaa Miandara <muqdara95@gmail.com>
2022-01-22 02:50:40 -0600

    fixed nav update 1

--- yolk/script.js
+++ yolk/script.js
@@ -1,5 +1,25 @@
 $(document).ready(function() {
   
+  var headerEl = document.querySelector("#fixedNav");
+  var sentinalEl = document.querySelector("#intersect");
+
+  function handler(entries) {
+    // entries is an array of observed dom nodes
+    // we're only interested in the first one at [0]
+    // because that's our .sentinal node.
+    // Here observe whether or not that node is in the viewport
+    if (!entries[0].isIntersecting) {
+      headerEl.classList.remove('hide');
+    } else {
+      headerEl.classList.add('hide');
+    }
+  }
+
+  // create the observer
+  var observer = new window.IntersectionObserver(handler);
+  // give the observer some dom nodes to keep an eye on
+  observer.observe(sentinalEl);
+
   $(function(){
     
     google.load("visualization", "1", {packages:["corechart"]});
