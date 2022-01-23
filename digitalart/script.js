349d49a65542faa29e012b2390acc04843d51603
Muqtadaa Miandara <muqdara95@gmail.com>
2022-01-22 22:18:50 -0600

    Digital Art Gallery Modal

--- digitalart/script.js
+++ digitalart/script.js
@@ -1,3 +1,19 @@
+const images = [
+  'url("./art/batman.jpg")',
+  'url("./art/dragonborn.png")',
+  'url("./art/eyepencil.jpg")',
+  'url("./art/galaxy.jpg")',
+  'url("./art/greenlantern.jpg")',
+  'url("./art/joker.png")',
+  'url("./art/obama.png")',
+  'url("./art/robot.jpg")',
+  'url("./art/selfportrait.jpg")',
+  'url("./art/spiderman.png")',
+  'url("./art/superman.png")',
+  'url("./art/theflash.jpg")',
+  'url("./art/womanpencil.jpg")'
+];
+
 window.onload = function () {
   var headerEl = document.querySelector("#fixedNav");
   var sentinalEl = document.querySelector("#intersect");
@@ -19,7 +35,23 @@ window.onload = function () {
   // give the observer some dom nodes to keep an eye on
   observer.observe(sentinalEl);
 
-  
+  var modal = document.getElementById('modal');
+
+  var modalClose = document.getElementById('modal');
+  modalClose.addEventListener('click', function () {
+    modal.style.display = "none";
+  });
+
+  document.addEventListener('click', function (e) { 
+    if (e.target.className.indexOf('gallery-image') !== -1) {
+        var img = e.target;
+        var modalImg = document.getElementById("modal-content");
+        var captionText = document.getElementById("modal-caption");
+        modal.style.display = "block";
+        modalImg.src = img.src;
+        captionText.innerHTML = img.alt;
+     }
+  });
 }
 
 $(document).ready(function () {
