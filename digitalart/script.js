859398e38dc7292ddeac85e6addd9e6418dc8a82
Muqtadaa Miandara <muqdara95@gmail.com>
2022-01-24 15:21:29 -0600

    left and right keys and swipe

--- digitalart/script.js
+++ digitalart/script.js
@@ -52,6 +52,116 @@ window.onload = function () {
       captionText.innerHTML = img.alt;
     }
   });
+  
+  var imageArr = document.querySelectorAll('.gallery-image');
+
+  function leftHandler() {
+    if (document.querySelector('#modal').style.display === 'block') {
+      var currentSrc = document.querySelector('#modal-content').getAttribute('src');
+      for (var i = 0; i < imageArr.length; i++) {
+        var compareSrc = imageArr[i].getAttribute('src').split('./').splice(1);
+        if (currentSrc.indexOf(compareSrc) !== -1) {
+          if (i === 0) {
+            document.querySelector('#modal-content').setAttribute('src', imageArr[imageArr.length - 1].getAttribute('src'));
+            document.querySelector('#modal-caption').innerText = imageArr[imageArr.length - 1].getAttribute('alt');
+          } else if (0 < i < imageArr.length) {
+            document.querySelector('#modal-content').setAttribute('src', imageArr[i - 1].getAttribute('src'));
+            document.querySelector('#modal-caption').innerText = imageArr[i - 1].getAttribute('alt');
+          }
+        }
+      }
+    }
+  }
+
+  function rightHandler() {
+    if (document.querySelector('#modal').style.display === 'block') {
+      var currentSrc = document.querySelector('#modal-content').getAttribute('src');
+      for (var i = 0; i < imageArr.length; i++) {
+        var compareSrc = imageArr[i].getAttribute('src').split('./').splice(1);
+        if (currentSrc.indexOf(compareSrc) !== -1) {
+          if (i === imageArr.length - 1) {
+            document.querySelector('#modal-content').setAttribute('src', imageArr[0].getAttribute('src'));
+            document.querySelector('#modal-caption').innerText = imageArr[0].getAttribute('alt');
+          } else if (0 < i < imageArr.length) {
+            document.querySelector('#modal-content').setAttribute('src', imageArr[i + 1].getAttribute('src'));
+            document.querySelector('#modal-caption').innerText = imageArr[i + 1].getAttribute('alt');
+          }
+        }
+      }
+    }
+  }
+
+  document.onkeydown = function (event) {
+    switch (event.keyCode) {
+      case 37:
+        leftHandler();
+        break;
+      case 39:
+        rightHandler();
+        break;
+      case 27:
+        modal.style.display = 'none';
+        break;
+    }
+  };
+
+  function swipedetect(el, callback) {
+
+    var touchsurface = el,
+      swipedir,
+      startX,
+      startY,
+      distX,
+      distY,
+      threshold = 150, //required min distance traveled to be considered swipe
+      restraint = 100, // maximum distance allowed at the same time in perpendicular direction
+      allowedTime = 300, // maximum time allowed to travel that distance
+      elapsedTime,
+      startTime,
+      handleswipe = callback || function (swipedir) { };
+
+    touchsurface.addEventListener('touchstart', function (e) {
+      var touchobj = e.changedTouches[0];
+      swipedir = 'none';
+      dist = 0;
+      startX = touchobj.pageX;
+      startY = touchobj.pageY;
+      startTime = new Date().getTime(); // record time when finger first makes contact with surface
+      e.preventDefault();
+    }, false);
+
+    touchsurface.addEventListener('touchmove', function (e) {
+      e.preventDefault(); // prevent scrolling when inside DIV
+    }, false);
+
+    touchsurface.addEventListener('touchend', function (e) {
+      var touchobj = e.changedTouches[0];
+      distX = touchobj.pageX - startX; // get horizontal dist traveled by finger while in contact with surface
+      distY = touchobj.pageY - startY; // get vertical dist traveled by finger while in contact with surface
+      elapsedTime = new Date().getTime() - startTime; // get time elapsed
+      if (elapsedTime <= allowedTime) { // first condition for awipe met
+        if (Math.abs(distX) >= threshold && Math.abs(distY) <= restraint) { // 2nd condition for horizontal swipe met
+          swipedir = (distX < 0) ? 'left' : 'right'; // if dist traveled is negative, it indicates left swipe
+        }
+        else if (Math.abs(distY) >= threshold && Math.abs(distX) <= restraint) { // 2nd condition for vertical swipe met
+          swipedir = (distY < 0) ? 'up' : 'down'; // if dist traveled is negative, it indicates up swipe
+        }
+      }
+      handleswipe(swipedir);
+      e.preventDefault();
+    }, false)
+  }
+
+  swipedetect(modal, function (swipedir) {
+    if (swipedir == 'left') {
+      rightHandler();
+    } else if(swipedir == 'right') {
+      leftHandler();
+    } else {
+      modal.style.display = 'none';
+    }
+  });
+
 }
 
 
